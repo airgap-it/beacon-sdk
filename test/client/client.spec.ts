ea67436ab2c4c77b202f720061dd1e3bc93ab738
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-12-09 14:26:06 +0100

    fix(test): stop test after 10 seconds

--- test/client/client.spec.ts
+++ test/client/client.spec.ts
@@ -1,8 +1,8 @@
-
-
 export class WalletCommunicationClientSpec {
-  public client = WalletCommunicationClient;
+  public client = WalletCommunicationClient
 }
+
+const MAX_TEST_RUNTIME_SECONDS = 10
 
 import * as chai from 'chai'
 import * as chaiAsPromised from 'chai-as-promised'
@@ -15,54 +15,75 @@ chai.use(chaiAsPromised)
 //const expect = chai.expect
 
 describe(`client - Custom Tests`, () => {
-  it("will connect to the p2p communication network", async () => {
-    const aliceClient = new WalletCommunicationClient("alice1234")
-    await aliceClient.start()
+  it('will connect to the p2p communication network', async () => {
+    return new Promise(async resolve => {
+      const intervals: NodeJS.Timeout[] = []
 
-    const bobClient = new WalletCommunicationClient("bob1234")
-    await bobClient.start()
+      const aliceClient = new WalletCommunicationClient('alice1234')
+      await aliceClient.start()
 
-    const charlieClient = new WalletCommunicationClient("charlie1234")
-    await charlieClient.start()
+      const bobClient = new WalletCommunicationClient('bob1234')
+      await bobClient.start()
 
-    aliceClient.listenForEncryptedMessage(bobClient.getPublicKey(), (message: string) => {
-      console.log("\n\nalice received from bob: " + message)
+      const charlieClient = new WalletCommunicationClient('charlie1234')
+      await charlieClient.start()
+
+      aliceClient.listenForEncryptedMessage(bobClient.getPublicKey(), (message: string) => {
+        console.log('\n\nalice received from bob: ' + message)
+      })
+
+      aliceClient.listenForEncryptedMessage(charlieClient.getPublicKey(), (message: string) => {
+        console.log('\n\nalice received from charlie: ' + message)
+      })
+
+      bobClient.listenForEncryptedMessage(aliceClient.getPublicKey(), (message: string) => {
+        console.log('\n\nbob received from alice: ' + message)
+      })
+
+      bobClient.listenForEncryptedMessage(charlieClient.getPublicKey(), (message: string) => {
+        console.log('\n\nbob received from charlie: ' + message)
+      })
+
+      charlieClient.listenForEncryptedMessage(aliceClient.getPublicKey(), (message: string) => {
+        console.log('\n\ncharlie received from alice: ' + message)
+      })
+
+      charlieClient.listenForEncryptedMessage(bobClient.getPublicKey(), (message: string) => {
+        console.log('\n\ncharlie received from bob: ' + message)
+      })
+
+      intervals.push(
+        setInterval(() => {
+          bobClient.sendMessage(aliceClient.getPublicKey(), `hey from bob ${Math.random()}\n\n`)
+          //bobClient.sendMessage(charlieClient.getPublicKey(), 'matrix-dev.papers.tech', "hey from bob")
+        }, 5000)
+      )
+
+      intervals.push(
+        setInterval(() => {
+          aliceClient.sendMessage(bobClient.getPublicKey(), `hey from alice ${Math.random()}\n\n`)
+          aliceClient.sendMessage(charlieClient.getPublicKey(), `hey from alice ${Math.random()}\n\n`)
+        }, 5000)
+      )
+
+      intervals.push(
+        setInterval(() => {
+          //charlieClient.sendMessage(bobClient.getPublicKey(), 'matrix.tez.ie', "hey from charlie")
+          charlieClient.sendMessage(aliceClient.getPublicKey(), `hey from charlie ${Math.random()}\n\n`)
+        }, 5000)
+      )
+
+      setTimeout(() => {
+        intervals.forEach(intervalId => {
+          console.log('clearing interval', intervalId)
+          clearInterval(intervalId)
+        })
+        resolve()
+
+        setTimeout(() => {
+          process.exit(0)
+        }, MAX_TEST_RUNTIME_SECONDS * 1000)
+      }, MAX_TEST_RUNTIME_SECONDS * 1000)
     })
-
-    aliceClient.listenForEncryptedMessage(charlieClient.getPublicKey(), (message: string) => {
-      console.log("\n\nalice received from charlie: " + message)
-    })
-
-    bobClient.listenForEncryptedMessage(aliceClient.getPublicKey(), (message: string) => {
-      console.log("\n\nbob received from alice: " + message)
-    })
-
-    bobClient.listenForEncryptedMessage(charlieClient.getPublicKey(), (message: string) => {
-      console.log("\n\nbob received from charlie: " + message)
-    })
-
-    charlieClient.listenForEncryptedMessage(aliceClient.getPublicKey(), (message: string) => {
-      console.log("\n\ncharlie received from alice: " + message)
-    })
-
-    charlieClient.listenForEncryptedMessage(bobClient.getPublicKey(), (message: string) => {
-      console.log("\n\ncharlie received from bob: " + message)
-    })
-
-    setInterval(() => {
-      bobClient.sendMessage(aliceClient.getPublicKey(), "hey from bob\n\n")
-      //bobClient.sendMessage(charlieClient.getPublicKey(), 'matrix-dev.papers.tech', "hey from bob")
-    }, 5000)
-
-    setInterval(() => {
-      aliceClient.sendMessage(bobClient.getPublicKey(), "hey from alice\n\n")
-      aliceClient.sendMessage(charlieClient.getPublicKey(), "hey from alice\n\n")
-    }, 5000)
-
-    setInterval(() => {
-      //charlieClient.sendMessage(bobClient.getPublicKey(), 'matrix.tez.ie', "hey from charlie")
-      charlieClient.sendMessage(aliceClient.getPublicKey(), "hey from charlie\n\n")
-    }, 5000)
-
   })
 })
