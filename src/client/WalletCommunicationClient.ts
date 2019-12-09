de183087a8b3444182345fd09f1d3f5a0edccc7a
Andreas Gassmann <andreas@andreasgassmann.ch>
2019-12-09 14:24:14 +0100

    feat(keypair): throw error if not available

--- src/client/WalletCommunicationClient.ts
+++ src/client/WalletCommunicationClient.ts
@@ -11,8 +11,17 @@ interface Room {
   currentState: any
 }
 
+interface KeyPair {
+  publicKey: string
+  privateKey: string
+}
+
+function getHexHash(key: string | Buffer): string {
+  return Buffer.from(sodium.crypto_generichash(32, key)).toString('hex')
+}
+
 export class WalletCommunicationClient {
-  private keyPair: any
+  private keyPair: KeyPair | undefined
   private clients: any[] = []
 
   private KNOWN_RELAY_SERVERS = [
@@ -23,9 +32,9 @@ export class WalletCommunicationClient {
     //"yadayada.cryptonomic-infra.tech"
   ]
 
-  constructor(public privateSeed = 'secret', public replicationCount = 3) {}
+  constructor(private readonly privateSeed: string = 'secret', public readonly replicationCount: number = 3) {}
 
-  public bigIntAbsolute(inputBigInt: any): BigInt {
+  private bigIntAbsolute(inputBigInt: any): BigInt {
     if (inputBigInt < 0n) {
       return inputBigInt * -1n
     } else {
@@ -33,20 +42,20 @@ export class WalletCommunicationClient {
     }
   }
 
-  private getAbsoluteBigIntDifference(firstHash: string, secondHash: string) {
+  private getAbsoluteBigIntDifference(firstHash: string, secondHash: string): BigInt {
     let difference = BigInt('0x' + firstHash) - BigInt('0x' + secondHash)
     return this.bigIntAbsolute(difference)
   }
 
-  public getRelayServer(
-    publicKeyHash: string = Buffer.from(sodium.crypto_generichash(32, this.keyPair.publicKey)).toString('hex'),
-    nonce: string = ''
-  ): string {
+  public getRelayServer(publicKeyHash?: string, nonce: string = ''): string {
+    if (!this.keyPair) {
+      throw new Error('KeyPair not available')
+    }
+    const hash: string = publicKeyHash || getHexHash(this.keyPair.publicKey)
     return this.KNOWN_RELAY_SERVERS.reduce((prev, curr) => {
-      const prevRelayServerHash = Buffer.from(sodium.crypto_generichash(32, prev + nonce)).toString('hex')
-      const currRelayServerHash = Buffer.from(sodium.crypto_generichash(32, curr + nonce)).toString('hex')
-      return this.getAbsoluteBigIntDifference(publicKeyHash, prevRelayServerHash) <
-        this.getAbsoluteBigIntDifference(publicKeyHash, currRelayServerHash)
+      const prevRelayServerHash = getHexHash(prev + nonce)
+      const currRelayServerHash = getHexHash(curr + nonce)
+      return this.getAbsoluteBigIntDifference(hash, prevRelayServerHash) < this.getAbsoluteBigIntDifference(hash, currRelayServerHash)
         ? prev
         : curr
     })
@@ -54,7 +63,8 @@ export class WalletCommunicationClient {
 
   public async start() {
     await sodium.ready
-    this.keyPair = sodium.crypto_sign_seed_keypair(sodium.crypto_generichash(32, sodium.from_string(this.privateSeed)))
+    const keyPair: KeyPair = sodium.crypto_sign_seed_keypair(sodium.crypto_generichash(32, sodium.from_string(this.privateSeed)))
+    this.keyPair = keyPair
 
     const publicKeyRawDigest = sodium.crypto_generichash(32, Buffer.from(this.keyPair.publicKey))
     const loginRawDigest = sodium.crypto_generichash(32, sodium.from_string('login:' + Math.floor(Date.now() / 1000 / (5 * 60))))
@@ -62,7 +72,7 @@ export class WalletCommunicationClient {
 
     //for (let i = 0; i < this.replicationCount; i++) {
     const client = matrixsdk.createClient({
-      baseUrl: 'https://' + this.getRelayServer(Buffer.from(sodium.crypto_generichash(32, this.keyPair.publicKey)).toString('hex'), '0'),
+      baseUrl: 'https://' + this.getRelayServer(getHexHash(this.keyPair.publicKey), '0'),
       deviceId: Buffer.from(this.keyPair.publicKey).toString('hex'),
       timelineSupport: false
     })
@@ -71,7 +81,7 @@ export class WalletCommunicationClient {
       'login',
       Buffer.from(publicKeyRawDigest).toString('hex'),
       'on',
-      this.getRelayServer(Buffer.from(sodium.crypto_generichash(32, this.keyPair.publicKey)).toString('hex'), '0')
+      this.getRelayServer(getHexHash(this.keyPair.publicKey), '0')
     )
     await client.login('m.login.password', {
       user: Buffer.from(publicKeyRawDigest).toString('hex'),
@@ -94,14 +104,23 @@ export class WalletCommunicationClient {
   }
 
   public getPublicKey(): string {
+    if (!this.keyPair) {
+      throw new Error('KeyPair not available')
+    }
     return Buffer.from(this.keyPair.publicKey).toString('hex')
   }
 
   public getPublicKeyHash(): string {
-    return Buffer.from(sodium.crypto_generichash(32, this.keyPair.publicKey)).toString('hex')
+    if (!this.keyPair) {
+      throw new Error('KeyPair not available')
+    }
+    return getHexHash(this.keyPair.publicKey)
   }
 
   public async listenForEncryptedMessage(senderPublicKey: string, messageCallback: (message: string) => void) {
+    if (!this.keyPair) {
+      throw new Error('KeyPair not available')
+    }
     const kxSelfPrivateKey = sodium.crypto_sign_ed25519_sk_to_curve25519(Buffer.from(this.keyPair.privateKey)) //secret bytes to scalar bytes
     const kxSelfPublicKey = sodium.crypto_sign_ed25519_pk_to_curve25519(Buffer.from(this.keyPair.privateKey).slice(32, 64)) //secret bytes to scalar bytes
     const kxOtherPublicKey = sodium.crypto_sign_ed25519_pk_to_curve25519(Buffer.from(senderPublicKey, 'hex')) //secret bytes to scalar bytes
@@ -113,12 +132,7 @@ export class WalletCommunicationClient {
 
     for (let client of this.clients) {
       client.on('event', (event: any) => {
-        if (
-          event.getType() === 'm.room.message' &&
-          event
-            .getSender()
-            .startsWith('@' + Buffer.from(sodium.crypto_generichash(32, Buffer.from(senderPublicKey, 'hex'))).toString('hex'))
-        ) {
+        if (event.getType() === 'm.room.message' && event.getSender().startsWith('@' + getHexHash(Buffer.from(senderPublicKey, 'hex')))) {
           let payload = Buffer.from(event.getContent().body, 'hex')
           if (payload.length >= sodium.crypto_secretbox_NONCEBYTES + sodium.crypto_secretbox_MACBYTES) {
             let nonce = payload.slice(0, sodium.crypto_secretbox_NONCEBYTES),
@@ -131,6 +145,9 @@ export class WalletCommunicationClient {
   }
 
   public async sendMessage(recipientPublicKey: string, message: string) {
+    if (!this.keyPair) {
+      throw new Error('KeyPair not available')
+    }
     const kxSelfPrivateKey = sodium.crypto_sign_ed25519_sk_to_curve25519(Buffer.from(this.keyPair.privateKey)) //secret bytes to scalar bytes
     const kxSelfPublicKey = sodium.crypto_sign_ed25519_pk_to_curve25519(Buffer.from(this.keyPair.privateKey).slice(32, 64)) //secret bytes to scalar bytes
     const kxOtherPublicKey = sodium.crypto_sign_ed25519_pk_to_curve25519(Buffer.from(recipientPublicKey, 'hex')) //secret bytes to scalar bytes
@@ -142,7 +159,7 @@ export class WalletCommunicationClient {
     )
 
     //for (let i = 0; i < this.replicationCount; i++) {
-    const recipientHash = Buffer.from(sodium.crypto_generichash(32, Buffer.from(recipientPublicKey, 'hex'))).toString('hex')
+    const recipientHash = getHexHash(Buffer.from(recipientPublicKey, 'hex'))
     const recipient = '@' + recipientHash + ':' + this.getRelayServer(recipientHash, '0')
     for (let client of this.clients) {
       const rooms = client.getRooms()
