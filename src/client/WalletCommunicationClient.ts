4081dc7e6bb6a6e00855785ac91e6d81a34c1374
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-01-14 14:00:44 +0000

    feat(qr): add ascii option

--- src/client/WalletCommunicationClient.ts
+++ src/client/WalletCommunicationClient.ts
@@ -40,7 +40,7 @@ export class WalletCommunicationClient {
     }
   }
 
-  public getHandshakeQR(type?: 'data' | 'svg'): string {
+  public getHandshakeQR(type?: 'data' | 'svg' | 'ascii'): string {
     const typeNumber: TypeNumber = 0
     const errorCorrectionLevel: ErrorCorrectionLevel = 'L'
     const qr = qrcode(typeNumber, errorCorrectionLevel)
@@ -49,6 +49,29 @@ export class WalletCommunicationClient {
       qr.make()
       if (type === 'svg') {
         return qr.createSvgTag()
+      } else if (type === 'ascii') {
+        const length = qr.getModuleCount()
+        const black = "\x1B[40m  \x1B[0m"
+        const white = "\x1B[47m  \x1B[0m"
+        const whiteLine = new Array(length + 3).join(white)
+        const blackLine = new Array(length + 3).join(black)
+        
+        let ascii = ''
+        ascii += blackLine + '\n';
+        ascii += whiteLine + '\n';
+        for (let x = 0; x < length; x++) {
+          ascii += white;
+
+          for (let y = 0; y < length; y++) {
+            ascii += qr.isDark(x, y) ? black : white
+          }
+
+          ascii += white + '\n';
+        }
+        ascii += whiteLine;
+        ascii += blackLine;
+
+        return ascii
       } else {
         return qr.createDataURL()
       }
