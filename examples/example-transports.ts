47b478f59027915b086672bc178a49009d8c8614
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-01-14 12:46:36 +0000

    feat(example): update example

--- examples/example-transports.ts
+++ examples/example-transports.ts
@@ -1,8 +1,32 @@
 import { DAppClient } from '../src/client/clients/DappClient'
 
 import { LocalTransport } from '../src/client/transports/LocalTransport'
+import { Serializer } from '../src/client/Serializer'
+import { MessageTypes, PermissionResponse } from '../src/client/Messages'
 
 const transport = new LocalTransport()
+
+// We emulate the wallet side and send a dummy response back
+transport.transformer = (message: string) => {
+  const serializer = new Serializer()
+  const deserialized: any = serializer.deserialize(message)
+  
+  if (deserialized.type === MessageTypes.PermissionRequest) {
+    const response: PermissionResponse = {
+      id: deserialized.id,
+      type: MessageTypes.PermissionResponse,
+      permissions: {
+        pubkey: 'dummy',
+        networks: ['mainnet'],
+        scopes: deserialized.scope,
+      }
+    }
+    return serializer.serialize(response)
+  }
+
+  return message
+}
+
 const client = new DAppClient('DApp')
 client
   .init(transport)
--- src/client/transports/LocalTransport.ts
+++ src/client/transports/LocalTransport.ts
@@ -1,7 +1,21 @@
+import { Logger } from '../utils/Logger'
 import { Transport } from './Transport'
+
+const logger = new Logger('LocalTransport')
 
 export class LocalTransport extends Transport {
   public static async isAvailable(): Promise<boolean> {
     return Promise.resolve(false)
+  }
+  
+  public transformer: (message: string) => string = (message: string) => message
+
+  public async send(message: string): Promise<void> {
+    logger.log('send', message)
+
+    const transformedMessage = this.transformer ? this.transformer(message) : message
+    await this.notifyListeners(transformedMessage)
+
+    return
   }
 }
