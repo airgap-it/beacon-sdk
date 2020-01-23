98e01c3fdf8c58d09348981afcd62fcbd1f70529
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-01-23 08:59:37 +0000

    feat(toast): add animation

--- src/client/alert/Toast.ts
+++ src/client/alert/Toast.ts
@@ -14,68 +14,104 @@ const getToastHTML = (config: ToastConfig): string => {
   const text = config.body
 
   return `
-  .beacon-toast__base {
-    position: absolute;
-    bottom: 16px;
-    right: 16px;
-    padding: 16px;
-    height: 64px;
-    z-index: 2147483000;
-    background: #fff;
-    margin: 0 auto;
-    border-radius: 4px;
-    overflow: hidden;
-    box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.1);
-    display: flex;
-    align-items: center;
-  }
+  <style>
+    :root {
+      --animation-duration: 300ms;
+    }
+    
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+    
+    @keyframes fadeOut {
+      from {
+        opacity: 1;
+      }
+      to {
+        opacity: 0;
+      }
+    }
+    
+    .animated {
+      animation-duration: var(--animation-duration);
+      animation-fill-mode: both;
+    }
+    
+    .fadeIn {
+      animation-name: fadeIn;
+    }
+    
+    .fadeOut {
+      animation-name: fadeOut;
+    }
 
-  .beacon-toast__content {
-    padding: 0 0 0 16px;
-    font-family: Roboto;
-  }
-  .spinner .loader,
-  .spinner .loader:after {
-    border-radius: 50%;
-    width: 32px;
-    height: 32px;
-  }
-  .spinner .loader {
-    position: relative;
-    text-indent: -9999em;
-    border-top: 4px solid rgba(56,128,255, 0.2);
-    border-right: 4px solid rgba(56,128,255, 0.2);
-    border-bottom: 4px solid rgba(56,128,255, 0.2);
-    border-left: 4px solid #3880ff;
-    -webkit-transform: translateZ(0);
-    -ms-transform: translateZ(0);
-    transform: translateZ(0);
-    -webkit-animation: spinner 1.1s infinite linear;
-    animation: spinner 1.1s infinite linear;
-  }
-  @-webkit-keyframes spinner {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
+    .beacon-toast__base {
+      position: absolute;
+      bottom: 16px;
+      right: 16px;
+      padding: 16px;
+      height: 64px;
+      z-index: 2147483000;
+      background: #fff;
+      margin: 0 auto;
+      border-radius: 4px;
+      overflow: hidden;
+      box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.1);
+      display: flex;
+      align-items: center;
     }
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
+
+    .beacon-toast__content {
+      padding: 0 0 0 16px;
+      font-family: Roboto;
     }
-  }
-  @keyframes spinner {
-    0% {
-      -webkit-transform: rotate(0deg);
-      transform: rotate(0deg);
+    .spinner .loader,
+    .spinner .loader:after {
+      border-radius: 50%;
+      width: 32px;
+      height: 32px;
     }
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
+    .spinner .loader {
+      position: relative;
+      text-indent: -9999em;
+      border-top: 4px solid rgba(56,128,255, 0.2);
+      border-right: 4px solid rgba(56,128,255, 0.2);
+      border-bottom: 4px solid rgba(56,128,255, 0.2);
+      border-left: 4px solid #3880ff;
+      -webkit-transform: translateZ(0);
+      -ms-transform: translateZ(0);
+      transform: translateZ(0);
+      -webkit-animation: spinner 1.1s infinite linear;
+      animation: spinner 1.1s infinite linear;
     }
-  }
-  
+    @-webkit-keyframes spinner {
+      0% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+    @keyframes spinner {
+      0% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
   </style>
-  <div id="beacon-toast" class="beacon-toast__base">
+  
+  <div id="beacon-toast" class="beacon-toast__base animated fadeIn">
     <div class="load-container spinner">
       <div class="loader">Loading...</div>
     </div>
