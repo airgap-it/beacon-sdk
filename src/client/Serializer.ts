2291feecbb9c6c7bffe98f6c34e7e724c4335c83
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-01-29 15:19:30 +0000

    feat(bs58check): add bs58check to serializer

--- src/client/Serializer.ts
+++ src/client/Serializer.ts
@@ -1,8 +1,22 @@
+import * as bs58check from 'bs58check'
+
 export class Serializer {
   public serialize(message: unknown): string {
-    return JSON.stringify(message)
+    const str = this.stringify(message)
+
+    return bs58check.encode(Buffer.from(str))
   }
   public deserialize(encoded: string): unknown {
-    return JSON.parse(encoded)
+    return JSON.parse(bs58check.decode(encoded))
+  }
+
+  private stringify(message: unknown): string {
+    return JSON.stringify(message, (_key, value) => {
+      if (typeof value === 'bigint') {
+        return value.toString()
+      } else {
+        return value // Return everything else unchanged
+      }
+    })
   }
 }
