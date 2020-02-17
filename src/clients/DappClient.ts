f90fb16f92f590afc78241367bca0b18020e6264
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-02-17 12:08:08 +0000

    feat(permissions): enforce permissions on dApp side

--- src/clients/DappClient.ts
+++ src/clients/DappClient.ts
@@ -6,7 +6,10 @@ import {
   SignPayloadResponse,
   BaseMessage,
   MessageTypes,
-  PermissionScope
+  PermissionScope,
+  BroadcastRequest,
+  OperationRequest,
+  SignPayloadRequest
 } from '../messages/Messages'
 import { ExposedPromise, exposedPromise } from '../utils/exposed-promise'
 
@@ -14,13 +17,15 @@ import { TezosOperation } from '../operations/OperationTypes'
 import { Logger } from '../utils/Logger'
 import { generateGUID } from '../utils/generate-uuid'
 import { TransportType } from '../transports/Transport'
-import { openToast } from '../alert/Toast'
+import { InternalEvent, InternalEventHandler } from '../events'
 import { BaseClient } from './Client'
 
 const logger = new Logger('DAppClient')
 
 export class DAppClient extends BaseClient {
+  private readonly events: InternalEventHandler = new InternalEventHandler()
   private readonly openRequests = new Map<string, ExposedPromise<Messages>>()
+  private readonly permissions: Map<string, string[]> = new Map<string, string[]>()
 
   public get isConnected(): Promise<boolean> {
     return this._isConnected.promise
@@ -81,20 +86,46 @@ export class DAppClient extends BaseClient {
     return super._connect()
   }
 
+  public async checkPermissions(type: MessageTypes): Promise<boolean> {
+    const permissions = this.permissions.get('')
+
+    if (!permissions) {
+      return false
+    }
+
+    switch (type) {
+      case MessageTypes.OperationRequest:
+        return permissions.some(permission => permission === 'operation_request')
+      case MessageTypes.SignPayloadRequest:
+        return permissions.some(permission => permission === 'sign')
+      case MessageTypes.BroadcastRequest:
+        return true
+      default:
+        return false
+    }
+  }
+
   public async requestPermissions(request?: PermissionScope[]): Promise<PermissionResponse> {
-    openToast({ body: 'Permission request sent', timer: 3000 }).catch(toastError =>
-      console.error(toastError)
-    )
+    this.events
+      .emit(InternalEvent.PERMISSION_REQUEST_SENT)
+      .catch(emitError => console.warn(emitError))
 
     return this.makeRequest<PermissionResponse>({
       id: '',
       name: this.name,
       type: MessageTypes.PermissionRequest,
       scope: request || ['read_address', 'sign', 'operation_request', 'threshold']
-    }).catch(error => {
-      console.log('error', error)
-      throw new Error(error)
     })
+      .catch(error => {
+        console.log('error', error)
+        throw new Error(error)
+      })
+      .then(async permissions => {
+        this.permissions.set('', permissions.permissions.scopes)
+        console.log('permissions interception', permissions)
+
+        return permissions
+      })
   }
 
   public async signPayloads(request: {
@@ -104,19 +135,30 @@ export class DAppClient extends BaseClient {
     if (!request.payload) {
       throw new Error('Payload must be provided')
     }
-    openToast({ body: 'Signing request sent', timer: 3000 }).catch(toastError =>
-      console.error(toastError)
-    )
 
-    return this.makeRequest<SignPayloadResponse>({
+    const req: SignPayloadRequest = {
       id: '',
       type: MessageTypes.SignPayloadRequest,
       payload: request.payload,
       sourceAddress: request.sourceAddress || ''
-    }).catch(error => {
-      console.log('error', error)
-      throw new Error(error)
-    })
+    }
+
+    if (await this.checkPermissions(req.type)) {
+      this.events.emit(InternalEvent.SIGN_REQUEST_SENT).catch(emitError => console.warn(emitError))
+
+      this.events
+        .emit(InternalEvent.OPERATION_REQUEST_SENT)
+        .catch(emitError => console.warn(emitError))
+
+      return this.makeRequest<SignPayloadResponse>(req).catch(error => {
+        console.log('error', error)
+        throw new Error(error)
+      })
+    } else {
+      this.events.emit(InternalEvent.SIGN_REQUEST_ERROR).catch(emitError => console.warn(emitError))
+
+      throw new Error('No permissions to send this request to wallet!')
+    }
   }
 
   public async requestOperation(request: {
@@ -126,19 +168,30 @@ export class DAppClient extends BaseClient {
     if (!request.operationDetails) {
       throw new Error('Operation details must be provided')
     }
-    openToast({ body: 'Operation request sent', timer: 3000 }).catch(toastError =>
-      console.error(toastError)
-    )
 
-    return this.makeRequest<OperationResponse>({
+    const req: OperationRequest = {
       id: '',
       type: MessageTypes.OperationRequest,
       network: request.network || 'mainnet',
       operationDetails: request.operationDetails
-    }).catch(error => {
-      console.log('error', error)
-      throw new Error(error)
-    })
+    }
+
+    if (await this.checkPermissions(req.type)) {
+      this.events
+        .emit(InternalEvent.OPERATION_REQUEST_SENT)
+        .catch(emitError => console.warn(emitError))
+
+      return this.makeRequest<OperationResponse>(req).catch(error => {
+        console.log('error', error)
+        throw new Error(error)
+      })
+    } else {
+      this.events
+        .emit(InternalEvent.OPERATION_REQUEST_ERROR)
+        .catch(emitError => console.warn(emitError))
+
+      throw new Error('No permissions to send this request to wallet!')
+    }
   }
 
   public async requestBroadcast(request: {
@@ -148,18 +201,29 @@ export class DAppClient extends BaseClient {
     if (!request.signedTransactions) {
       throw new Error('Operation details must be provided')
     }
-    openToast({ body: 'Broadcast request sent', timer: 3000 }).catch(toastError =>
-      console.error(toastError)
-    )
 
-    return this.makeRequest<BroadcastResponse>({
+    const req: BroadcastRequest = {
       id: '',
       type: MessageTypes.BroadcastRequest,
       network: request.network || 'mainnet',
       signedTransactions: request.signedTransactions
-    }).catch(error => {
-      console.log('error', error)
-      throw new Error(error)
-    })
+    }
+
+    if (await this.checkPermissions(req.type)) {
+      this.events
+        .emit(InternalEvent.BROADCAST_REQUEST_SENT)
+        .catch(emitError => console.warn(emitError))
+
+      return this.makeRequest<BroadcastResponse>(req).catch(error => {
+        console.log('error', error)
+        throw new Error(error)
+      })
+    } else {
+      this.events
+        .emit(InternalEvent.BROADCAST_REQUEST_ERROR)
+        .catch(emitError => console.warn(emitError))
+
+      throw new Error('No permissions to send this request to wallet!')
+    }
   }
 }
