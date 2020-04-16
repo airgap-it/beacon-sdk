0de1f531c50a7b414aa933f9496c257dcf285691
AndreasGassmann <andreas@andreasgassmann.ch>
2020-04-16 15:34:32 +0000

    feat(docs): add deploy script

---
+++ deploy-docs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'docs.walletbeacon.io' > CNAME
+
+git init
+git add -A
+git commit -m 'chore(docs): including latest changes'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:airgap-it/beacon-sdk.git master:gh-pages
+
+cd -
