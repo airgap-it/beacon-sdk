5368a6f18fca5724765a89ac11953ae2ce99e925
AndreasGassmann <andreas@andreasgassmann.ch>
2020-04-22 05:34:14 +0000

    feat(version): add version to beacon messages

--- src/types/beacon/BeaconBaseMessage.ts
+++ src/types/beacon/BeaconBaseMessage.ts
@@ -2,6 +2,7 @@ import { BeaconMessageType } from '../..'
 
 export interface BeaconBaseMessage {
   type: BeaconMessageType
+  version: string
   id: string // ID of the message. The same ID is used in the request and response
   beaconId: string // ID of the sender. This is used to identify the sender of the message
   // TODO: The beaconID (which is a public key) can be used to verify a signature and prevent message spoofing
