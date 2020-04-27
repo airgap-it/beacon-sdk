8df7e84b0659168d2975ae99d0f23f445137a563
AndreasGassmann <andreas@andreasgassmann.ch>
2020-04-27 16:22:56 +0000

    chore(example): add example how to override QR alert

---
+++ src/examples/override-ui-events.ts
@@ -0,0 +1,39 @@
+import {
+  BeaconErrorMessage,
+  DAppClient,
+  PermissionScope,
+  PermissionResponseOutput,
+  P2PPairInfo
+} from '..'
+import { BeaconEvent, defaultEventCallbacks } from '../events'
+
+const client = new DAppClient({
+  name: 'My Sample DApp',
+  eventHandlers: {
+    [BeaconEvent.P2P_LISTEN_FOR_CHANNEL_OPEN]: {
+      // Every BeaconEvent can be overriden by passing a handler here.
+      // The default will not be executed anymore. To keep the default,
+      // you will have to call it again.
+      handler: async (syncInfo: P2PPairInfo): Promise<void> => {
+        await defaultEventCallbacks.P2P_LISTEN_FOR_CHANNEL_OPEN(syncInfo) // Add this if you want to keep the default behaviour.
+        console.log('syncInfo', syncInfo)
+      }
+    } as any // TODO: Fix type
+  }
+})
+
+const scopes: PermissionScope[] = [
+  PermissionScope.READ_ADDRESS,
+  PermissionScope.OPERATION_REQUEST,
+  PermissionScope.SIGN,
+  PermissionScope.THRESHOLD
+]
+
+client
+  .requestPermissions({
+    scopes
+  })
+  .then((permissionResponse: PermissionResponseOutput) => {
+    console.log('permissions', permissionResponse)
+  })
+  .catch((permissionError: BeaconErrorMessage) => console.error(permissionError))
