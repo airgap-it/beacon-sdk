002d8d5effeb34d44ac8229b4a2eeb53111e6fa4
AndreasGassmann <andreas@andreasgassmann.ch>
2020-04-27 12:37:48 +0000

    feat(events): add default handler

--- src/events.ts
+++ src/events.ts
@@ -1,7 +1,7 @@
 import { openToast } from './alert/Toast'
 import { openAlert } from './alert/Alert'
 
-export enum BeaconEvents {
+export enum BeaconEvent {
   PERMISSION_REQUEST_SENT = 'PERMISSION_REQUEST_SENT',
   PERMISSION_REQUEST_ERROR = 'PERMISSION_REQUEST_ERROR',
   OPERATION_REQUEST_SENT = 'OPERATION_REQUEST_SENT',
@@ -16,10 +16,6 @@ export enum BeaconEvents {
   ACTIVE_ACCOUNT_SET = 'ACTIVE_ACCOUNT_SET',
 
   UNKNOWN = 'UNKNOWN'
-}
-
-const listenerFallback = async (data?: unknown): Promise<void> => {
-  console.error(`no default listener for event ${event}`, data)
 }
 
 const showSentToast = async (): Promise<void> => {
@@ -40,50 +36,50 @@ const showRateLimitReached = async (): Promise<void> => {
   }).catch((toastError) => console.error(toastError))
 }
 
-const emptyHandler = async (): Promise<void> => {
-  /* Do nothing */
+const emptyHandler = async (data?: unknown): Promise<void> => {
+  console.error(`no default listener for event ${event}`, data)
 }
 
 export type BeaconEventHandlerFunction = (data?: unknown) => Promise<void>
 
-export const defaultEventCallbacks: { [key in BeaconEvents]: BeaconEventHandlerFunction } = {
-  [BeaconEvents.PERMISSION_REQUEST_SENT]: showSentToast,
-  [BeaconEvents.PERMISSION_REQUEST_ERROR]: showNoPermissionAlert,
-  [BeaconEvents.OPERATION_REQUEST_SENT]: showSentToast,
-  [BeaconEvents.OPERATION_REQUEST_ERROR]: showNoPermissionAlert,
-  [BeaconEvents.SIGN_REQUEST_SENT]: showSentToast,
-  [BeaconEvents.SIGN_REQUEST_ERROR]: showNoPermissionAlert,
-  [BeaconEvents.BROADCAST_REQUEST_SENT]: showSentToast,
-  [BeaconEvents.BROADCAST_REQUEST_ERROR]: showNoPermissionAlert,
-  [BeaconEvents.LOCAL_RATE_LIMIT_REACHED]: showRateLimitReached,
-  [BeaconEvents.NO_PERMISSIONS]: showNoPermissionAlert,
-  [BeaconEvents.ACTIVE_ACCOUNT_SET]: emptyHandler,
-  [BeaconEvents.UNKNOWN]: emptyHandler
+export const defaultEventCallbacks: { [key in BeaconEvent]: BeaconEventHandlerFunction } = {
+  [BeaconEvent.PERMISSION_REQUEST_SENT]: showSentToast,
+  [BeaconEvent.PERMISSION_REQUEST_ERROR]: showNoPermissionAlert,
+  [BeaconEvent.OPERATION_REQUEST_SENT]: showSentToast,
+  [BeaconEvent.OPERATION_REQUEST_ERROR]: showNoPermissionAlert,
+  [BeaconEvent.SIGN_REQUEST_SENT]: showSentToast,
+  [BeaconEvent.SIGN_REQUEST_ERROR]: showNoPermissionAlert,
+  [BeaconEvent.BROADCAST_REQUEST_SENT]: showSentToast,
+  [BeaconEvent.BROADCAST_REQUEST_ERROR]: showNoPermissionAlert,
+  [BeaconEvent.LOCAL_RATE_LIMIT_REACHED]: showRateLimitReached,
+  [BeaconEvent.NO_PERMISSIONS]: showNoPermissionAlert,
+  [BeaconEvent.ACTIVE_ACCOUNT_SET]: emptyHandler,
+  [BeaconEvent.UNKNOWN]: emptyHandler
 }
 
 export class BeaconEventHandler {
-  private readonly callbackMap: { [key in BeaconEvents]: BeaconEventHandlerFunction[] } = {
-    [BeaconEvents.PERMISSION_REQUEST_SENT]: [],
-    [BeaconEvents.PERMISSION_REQUEST_ERROR]: [],
-    [BeaconEvents.OPERATION_REQUEST_SENT]: [],
-    [BeaconEvents.OPERATION_REQUEST_ERROR]: [],
-    [BeaconEvents.SIGN_REQUEST_SENT]: [],
-    [BeaconEvents.SIGN_REQUEST_ERROR]: [],
-    [BeaconEvents.BROADCAST_REQUEST_SENT]: [],
-    [BeaconEvents.BROADCAST_REQUEST_ERROR]: [],
-    [BeaconEvents.LOCAL_RATE_LIMIT_REACHED]: [],
-    [BeaconEvents.NO_PERMISSIONS]: [],
-    [BeaconEvents.ACTIVE_ACCOUNT_SET]: [],
-    [BeaconEvents.UNKNOWN]: []
+  private readonly callbackMap: { [key in BeaconEvent]: BeaconEventHandlerFunction[] } = {
+    [BeaconEvent.PERMISSION_REQUEST_SENT]: [defaultEventCallbacks.PERMISSION_REQUEST_SENT],
+    [BeaconEvent.PERMISSION_REQUEST_ERROR]: [defaultEventCallbacks.PERMISSION_REQUEST_ERROR],
+    [BeaconEvent.OPERATION_REQUEST_SENT]: [defaultEventCallbacks.OPERATION_REQUEST_SENT],
+    [BeaconEvent.OPERATION_REQUEST_ERROR]: [defaultEventCallbacks.OPERATION_REQUEST_ERROR],
+    [BeaconEvent.SIGN_REQUEST_SENT]: [defaultEventCallbacks.SIGN_REQUEST_SENT],
+    [BeaconEvent.SIGN_REQUEST_ERROR]: [defaultEventCallbacks.SIGN_REQUEST_ERROR],
+    [BeaconEvent.BROADCAST_REQUEST_SENT]: [defaultEventCallbacks.BROADCAST_REQUEST_SENT],
+    [BeaconEvent.BROADCAST_REQUEST_ERROR]: [defaultEventCallbacks.BROADCAST_REQUEST_ERROR],
+    [BeaconEvent.LOCAL_RATE_LIMIT_REACHED]: [defaultEventCallbacks.LOCAL_RATE_LIMIT_REACHED],
+    [BeaconEvent.NO_PERMISSIONS]: [defaultEventCallbacks.NO_PERMISSIONS],
+    [BeaconEvent.ACTIVE_ACCOUNT_SET]: [defaultEventCallbacks.ACTIVE_ACCOUNT_SET],
+    [BeaconEvent.UNKNOWN]: [defaultEventCallbacks.UNKNOWN]
   }
 
-  public async on(event: BeaconEvents, eventCallback: BeaconEventHandlerFunction): Promise<void> {
+  public async on(event: BeaconEvent, eventCallback: BeaconEventHandlerFunction): Promise<void> {
     const listeners = this.callbackMap[event] || []
     listeners.push(eventCallback)
     this.callbackMap[event] = listeners
   }
 
-  public async emit(event: BeaconEvents, data?: unknown): Promise<void> {
+  public async emit(event: BeaconEvent, data?: unknown): Promise<void> {
     const listeners = this.callbackMap[event]
     if (listeners && listeners.length > 0) {
       listeners.forEach((listener) => {
@@ -91,22 +87,19 @@ export class BeaconEventHandler {
           console.error(`error handling event ${event}`, listenerError)
         )
       })
-    } else {
-      const listener = defaultEventCallbacks[event] || listenerFallback
-      await listener(data)
     }
   }
 
   public async overrideDefaults(
     eventsToOverride: {
-      [key in BeaconEvents]: {
+      [key in BeaconEvent]: {
         handler: BeaconEventHandlerFunction
       }
     }
   ): Promise<void> {
     Object.keys(eventsToOverride).forEach((untypedEvent: string) => {
-      const event: BeaconEvents = untypedEvent as BeaconEvents
-      this.callbackMap[event].push(eventsToOverride[event].handler)
+      const event: BeaconEvent = untypedEvent as BeaconEvent
+      this.callbackMap[event] = [eventsToOverride[event].handler]
     })
   }
 }
