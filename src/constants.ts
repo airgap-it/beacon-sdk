1153fcad70d6d3fd40db07dcdc86e0c6c8b02e5b
AndreasGassmann <andreas@andreasgassmann.ch>
2020-04-29 08:50:39 +0000

    feat(version): update beacon message version

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const SDK_VERSION = '0.1.0'
-export const BEACON_VERSION = '0.1' // TODO: Update to 1 once we have the final release
+export const SDK_VERSION: string = '0.1.0'
+export const BEACON_VERSION: string = '1'
