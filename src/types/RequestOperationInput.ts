c904498c525aac36aa6c4f112d9a025ae37c856a
AndreasGassmann <andreas@andreasgassmann.ch>
2020-07-10 13:42:39 +0000

    feat(operation-request): remove option to overwrite network

--- src/types/RequestOperationInput.ts
+++ src/types/RequestOperationInput.ts
@@ -1,7 +1,5 @@
-import { Network } from '..'
 import { PartialTezosOperation } from './tezos/PartialTezosOperation'
 
 export interface RequestOperationInput {
-  network?: Network
   operationDetails: PartialTezosOperation[]
 }
