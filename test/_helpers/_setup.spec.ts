e6d212e287ccc2253d12132990ea3d6ddde2f03b
AndreasGassmann <andreas@andreasgassmann.ch>
2020-09-02 07:05:56 +0000

    feat(test): mock all api requests

--- test/_helpers/_setup.spec.ts
+++ test/_helpers/_setup.spec.ts
@@ -1,9 +1,22 @@
 import { MockLocalStorage } from '../test-utils/MockLocalStorage'
 ;(global as any).localStorage = new MockLocalStorage()
 
-// require('jsdom-global')()
+import Axios from 'axios'
+const MockAdapter = require('axios-mock-adapter')
 
+/**
+ * Create a JSDOM instance to support localStorage and other DOM methods
+ */
 const { JSDOM } = require('jsdom')
 new JSDOM('<!doctype html><html><body></body></html>', {
   url: 'http://localhost/'
+})
+
+// This sets the mock adapter on the default instance
+const mock = new MockAdapter(Axios)
+
+mock.onAny().replyOnce((config) => {
+  console.log('UNMOCKED URL, RETURNING ERROR 500', config.url)
+
+  return [500, {}]
 })
