e8666e339b771b89dd606380c52f61922f95684e
AndreasGassmann <andreas@andreasgassmann.ch>
2020-09-02 07:06:41 +0000

    feat(wallet-client-option): add storage to options

--- src/clients/wallet-client/WalletClientOptions.ts
+++ src/clients/wallet-client/WalletClientOptions.ts
@@ -1,8 +1,14 @@
+import { Storage } from '../..'
+
 export interface WalletClientOptions {
   /**
    * Name of the application
    */
   name: string
+  /**
+   * The storage that will be used by the SDK
+   */
+  storage?: Storage
   /**
    * A list of matrix nodes the application can use to connect to
    */
