723e232af3005ef7f3e265d9528cd93c08a074b2
AndreasGassmann <andreas@andreasgassmann.ch>
2020-11-25 15:49:03 +0100

    chore(release): 2.0.0-beta.15

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const SDK_VERSION: string = '2.0.0-beta.14'
+export const SDK_VERSION: string = '2.0.0-beta.15'
 export const BEACON_VERSION: string = '2'
 export const DEBUG: boolean = false
