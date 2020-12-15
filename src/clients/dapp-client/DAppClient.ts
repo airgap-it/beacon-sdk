4e57cafee5c59610d176e5e0a3dc89ef172ca988
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-12-15 10:15:33 +0000

    fix(dapp-client): init did not resolve if called multiple times

--- src/clients/dapp-client/DAppClient.ts
+++ src/clients/dapp-client/DAppClient.ts
@@ -106,6 +106,8 @@ export class DAppClient extends Client {
     ExtendedPostMessagePairingResponse | ExtendedP2PPairingResponse | undefined
   > = new ExposedPromise()
 
+  private _initPromise: Promise<TransportType> | undefined
+
   constructor(config: DAppClientOptions) {
     super({
       storage: config && config.storage ? config.storage : new LocalStorage(),
@@ -175,7 +177,11 @@ export class DAppClient extends Client {
   }
 
   public async init(transport?: Transport<any>): Promise<TransportType> {
-    return new Promise(async (resolve) => {
+    if (this._initPromise) {
+      return this._initPromise
+    }
+
+    this._initPromise = new Promise(async (resolve) => {
       if (transport) {
         await this.addListener(transport)
 
@@ -266,6 +272,8 @@ export class DAppClient extends Client {
         }
       }
     })
+
+    return this._initPromise
   }
 
   /**
