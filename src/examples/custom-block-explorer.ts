4bbb8ebfbd1e256346c18d9c4380d33bceb49a41
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-12-18 08:40:52 +0000

    docs(blockexplorer): add example with tzstats-blockexplorer

---
+++ src/examples/custom-block-explorer.ts
@@ -0,0 +1,36 @@
+import { ErrorResponse, DAppClient, PermissionResponseOutput, NetworkType, Network } from '..' // Replace '..' with '@airgap/beacon-sdk'
+import { BlockExplorer } from '../utils/block-explorer'
+
+class TzStatsBlockExplorer extends BlockExplorer {
+  constructor(
+    public readonly rpcUrls: { [key in NetworkType]: string } = {
+      [NetworkType.MAINNET]: 'https://tzstats.com/',
+      [NetworkType.CARTHAGENET]: 'https://carthagenet.tzstats.com/',
+      [NetworkType.DELPHINET]: 'https://delphi.tzstats.com/',
+      // [NetworkType.EDONET]: 'https://edo.tzstats.com/',
+      [NetworkType.CUSTOM]: 'https://edo.tzstats.com/'
+    }
+  ) {
+    super(rpcUrls)
+  }
+
+  public async getAddressLink(address: string, network: Network): Promise<string> {
+    const blockExplorer = await this.getLinkForNetwork(network)
+
+    return `${blockExplorer}/${address}`
+  }
+  public async getTransactionLink(transactionId: string, network: Network): Promise<string> {
+    const blockExplorer = await this.getLinkForNetwork(network)
+
+    return `${blockExplorer}/${transactionId}`
+  }
+}
+
+const client = new DAppClient({ name: 'My Sample DApp', blockExplorer: new TzStatsBlockExplorer() })
+
+client
+  .requestPermissions()
+  .then((response: PermissionResponseOutput) => {
+    console.log('permissions', response)
+  })
+  .catch((permissionError: ErrorResponse) => console.error(permissionError))
