cf52f0cd47c8945cef2612d9b694bcfebd3906d9
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-06-03 10:54:40 +0200

    fix(test): stop client after one second

--- test/matrix-client/matrix-client.spec.ts
+++ test/matrix-client/matrix-client.spec.ts
@@ -282,6 +282,11 @@ describe(`MatrixClient`, () => {
       const successResponse = 'my-response'
       const getStub = sinon.stub((<any>client).store, 'get').returns(3)
       const syncStub = sinon.stub(client, <any>'sync').returns(successResponse)
+
+      // Stop the requests after 1s, otherwise it will retry forever
+      setTimeout(() => {
+        client.stop()
+      }, 1000)
       ;(<any>client)
         .poll(
           0,
