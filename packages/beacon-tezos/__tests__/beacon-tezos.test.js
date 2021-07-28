1a32e1a9e38dfb061e4c16b2cf2e5bd450ec7892
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-07-28 15:21:39 +0200

    feat(lerna): change default files

--- packages/beacon-tezos/__tests__/beacon-tezos.test.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const beaconTezos = require('..')
-
-describe('@airgap/beacon-tezos', () => {
-  it('needs tests')
-})
--- packages/beacon-tezos/lib/beacon-tezos.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = beaconTezos
-
-function beaconTezos() {
-  // TODO
-}
