8aededfc1bed5e97cbefda55344074bdb9aec3c5
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-11-10 10:15:19 +0100

    feat(network) add idiazabalnet

--- src/ui/alert/Pairing.ts
+++ src/ui/alert/Pairing.ts
@@ -54,6 +54,7 @@ export interface WebApp extends AppBase {
     [NetworkType.FLORENCENET]?: string
     [NetworkType.GRANADANET]?: string
     [NetworkType.HANGZHOUNET]?: string
+    [NetworkType.IDIAZABALNET]?: string
     [NetworkType.CUSTOM]?: string
   }
 }
--- src/utils/tezblock-blockexplorer.ts
+++ src/utils/tezblock-blockexplorer.ts
@@ -10,6 +10,7 @@ export class TezblockBlockExplorer extends BlockExplorer {
       [NetworkType.FLORENCENET]: 'https://florencenet.tezblock.io',
       [NetworkType.GRANADANET]: 'https://granadanet.tezblock.io',
       [NetworkType.HANGZHOUNET]: 'https://hangzhounet.tezblock.io',
+      [NetworkType.IDIAZABALNET]: 'https://idiazabalnet.tezblock.io',
       [NetworkType.CUSTOM]: 'https://granadanet.tezblock.io'
     }
   ) {
