b599206d3f9a2b9f94ddf6024fcb3f2342e09960
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-11-11 13:24:02 +0100

    feat(build): check version

--- packages/beacon-core/src/constants.ts
+++ packages/beacon-core/src/constants.ts
@@ -1,2 +1,2 @@
-export const SDK_VERSION: string = '2.3.6'
+export const SDK_VERSION: string = '0.0.1-beta.12'
 export const BEACON_VERSION: string = '2'
--- scripts/check-sdk-version.ts
+++ scripts/check-sdk-version.ts
@@ -1,20 +1,12 @@
 import { readFileSync } from 'fs'
 import { SDK_VERSION } from '../packages/beacon-core/src/constants'
 
-const packageJson = './package.json'
-const packageLockJson = './package-lock.json'
+const lernaJson = './lerna.json'
 
-const packageJsonContent = JSON.parse(readFileSync(packageJson, 'utf-8'))
-const packageLockJsonContent = JSON.parse(readFileSync(packageLockJson, 'utf-8'))
+const lernaJsonContent = JSON.parse(readFileSync(lernaJson, 'utf-8'))
 
-if (packageJsonContent.version !== packageLockJsonContent.version) {
+if (lernaJsonContent.version !== SDK_VERSION) {
   throw new Error(
-    `Package (${packageJsonContent.version}) and Package Lock (${packageLockJsonContent.version}) version mismatch!`
-  )
-}
-
-if (packageJsonContent.version !== SDK_VERSION) {
-  throw new Error(
-    `Package version (${packageJsonContent.version}) does not match SDK Version (${SDK_VERSION})`
+    `Version in lerna.json (${lernaJsonContent.version}) does not match SDK Version (${SDK_VERSION})`
   )
 }
