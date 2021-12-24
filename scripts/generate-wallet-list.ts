068063962657e93e9d20b0f54b3fcf0839717677
thuyentruong <thuyen@bitmark.com>
2021-12-24 15:53:37 +0700

    feat(wallet): added autonomy wallet support

--- scripts/generate-wallet-list.ts
+++ scripts/generate-wallet-list.ts
@@ -80,6 +80,15 @@ export const iosList: App[] = [
     logo: 'ios-naan.png',
     universalLink: 'https://naanwallet.com',
     deepLink: 'naan://'
+  },
+  {
+    key: 'autonomy-app',
+    name: 'Autonomy',
+    shortName: 'Autonomy',
+    color: '',
+    logo: 'ios-autonomy.png',
+    universalLink: 'https://au.bitmark.com/apps/tezos',
+    deepLink: 'autonomy-tezos://'
   }
   // {
   //   name: 'Galleon',
