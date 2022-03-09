9928ab14964a893d3dab89c474244641fb7011ac
Andreas Gassmann <andreas@andreasgassmann.ch>
2022-03-09 18:17:13 +0100

    feat(): unify messages

--- packages/beacon-blockchain-kusama/src/messages/operation-request.ts
+++ packages/beacon-blockchain-kusama/src/messages/operation-request.ts
@@ -4,11 +4,15 @@ export interface NewPermissionRequest<T extends string> {
   blockchainIdentifier: T
 }
 
-// Those are example permissions
 export enum SubstratePermissionScope {
   'transfer',
   'sign_payload_json',
   'sign_payload_raw'
+}
+
+export enum SubstrateMessageType {
+  'transfer_request',
+  'sign_payload_request'
 }
 
 export interface SubstratePermissionRequest extends NewPermissionRequest<'substrate'> {
@@ -38,6 +42,7 @@ export interface SubstratePermissionResponse extends PermissionResponseV3<'subst
 export interface SubstrateTransferRequest extends BlockchainMessage<'substrate'> {
   blockchainData: {
     scope: SubstratePermissionScope.transfer
+    type: SubstrateMessageType.transfer_request
     sourceAddress: string
     amount: string
     recipient: string
@@ -64,101 +69,80 @@ export type SubstrateTransferResponse =
 
 export interface SubstrateSignPayloadRequest extends BlockchainMessage<'substrate'> {
   blockchainData: {
-    scope: SubstratePermissionScope.sign_payload_json
-    mode: 'submit' | 'submit-and-return' | 'return' // TODO: Wording
+    type: SubstrateMessageType.sign_payload_request
+    scope: SubstratePermissionScope.sign_payload_json | SubstratePermissionScope.sign_payload_json
 
     // This type is the same as the "SignerPayloadJSON" of polkadot.js https://github.com/polkadot-js/api/blob/f169ca08a80ea9c3865dc545e03e921c50f0d284/packages/types/src/types/extrinsic.ts#L32
-    data: {
-      /**
-       * @description The ss-58 encoded address
-       */
-      address: string
+    payload:
+      | {
+          type: 'json'
 
-      /**
-       * @description The checkpoint hash of the block, in hex
-       */
-      blockHash: string
+          /**
+           * @description The checkpoint hash of the block, in hex
+           */
+          blockHash: string
 
-      /**
-       * @description The checkpoint block number, in hex
-       */
-      blockNumber: string
+          /**
+           * @description The checkpoint block number, in hex
+           */
+          blockNumber: string
 
-      /**
-       * @description The era for this transaction, in hex
-       */
-      era: string
+          /**
+           * @description The era for this transaction, in hex
+           */
+          era: string
 
-      /**
-       * @description The genesis hash of the chain, in hex
-       */
-      genesisHash: string
+          /**
+           * @description The genesis hash of the chain, in hex
+           */
+          genesisHash: string
 
-      /**
-       * @description The encoded method (with arguments) in hex
-       */
-      method: string
+          /**
+           * @description The encoded method (with arguments) in hex
+           */
+          method: string
 
-      /**
-       * @description The nonce for this transaction, in hex
-       */
-      nonce: string
+          /**
+           * @description The nonce for this transaction, in hex
+           */
+          nonce: string
 
-      /**
-       * @description The current spec version for the runtime
-       */
-      specVersion: string
+          /**
+           * @description The current spec version for the runtime
+           */
+          specVersion: string
 
-      /**
-       * @description The tip for this transaction, in hex
-       */
-      tip: string
+          /**
+           * @description The tip for this transaction, in hex
+           */
+          tip: string
 
-      /**
-       * @description The current transaction version for the runtime
-       */
-      transactionVersion: string
+          /**
+           * @description The current transaction version for the runtime
+           */
+          transactionVersion: string
 
-      /**
-       * @description The applicable signed extensions for this runtime
-       */
-      signedExtensions: string[]
+          /**
+           * @description The applicable signed extensions for this runtime
+           */
+          signedExtensions: string[]
 
-      /**
-       * @description The version of the extrinsic we are dealing with
-       */
-      version: number
-    }
+          /**
+           * @description The version of the extrinsic we are dealing with
+           */
+          version: number
+        }
+      | {
+          type: 'raw'
+          isMutable: boolean
+          dataType: 'bytes' | 'payload'
+          data: string
+        }
+
+    mode: 'submit' | 'submit-and-return' | 'return'
   }
 }
 export type SubstrateSignPayloadResponse =
-  | {
-      transactionHash: string
-    }
-  | {
-      transactionHash: string
-      signature: string
-      payload?: string
-    }
-  | {
-      signature: string
-      payload?: string
-    }
-
-export interface SubstrateSignRequest extends BlockchainMessage<'substrate'> {
-  blockchainData: {
-    scope: SubstratePermissionScope.sign_payload_raw
-
-    payload: {
-      isMutable: boolean
-      type: 'bytes' | 'payload'
-      data: string
-    }
-
-    mode: 'submit' | 'submit-and-return' | 'return' // TODO: Wording
-  }
-}
-export type SubstrateSignResponse =
   | {
       transactionHash: string
     }
