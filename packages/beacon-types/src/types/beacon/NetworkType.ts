9d807b34e2943b688b7755dac568e2b29e4061a9
Andreas Gassmann <andreas@andreasgassmann.ch>
2022-06-29 16:16:52 -0300

    feat(): add ghostnet

--- packages/beacon-types/src/types/beacon/NetworkType.ts
+++ packages/beacon-types/src/types/beacon/NetworkType.ts
@@ -1,5 +1,8 @@
 export enum NetworkType {
   MAINNET = 'mainnet',
+  GHOSTNET = 'ghostnet', // Long running testnet
+  MONDAYNET = 'mondaynet', // Testnet, resets every monday
+  DAILYNET = 'mondaynet', // Testnet, resets every day
   DELPHINET = 'delphinet',
   EDONET = 'edonet',
   FLORENCENET = 'florencenet',
