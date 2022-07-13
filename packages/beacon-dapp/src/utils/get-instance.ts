9c3595be4348f7fa0635572ded68ff6839740258
Andreas Gassmann <andreas@andreasgassmann.ch>
2022-07-13 13:28:09 +0200

    feat(singleton): remove local config

--- packages/beacon-dapp/src/utils/get-instance.ts
+++ packages/beacon-dapp/src/utils/get-instance.ts
@@ -1,14 +1,12 @@
 import { DAppClient, DAppClientOptions } from '..'
 
 let _instance: DAppClient | undefined
-let _config: DAppClientOptions | undefined
 
 /** Get a DAppClient instance. Will make sure only one dAppClient exists. After the first instance has been created, the config will be ingored, unless "reset" is set */
 export const getDAppClientInstance = (config: DAppClientOptions, reset: boolean): DAppClient => {
   if (_instance && reset) {
     _instance.disconnect()
     _instance = undefined
-    _config = undefined
   }
 
   if (_instance) {
@@ -17,7 +15,6 @@ export const getDAppClientInstance = (config: DAppClientOptions, reset: boolean)
 
   if (!_instance) {
     _instance = new DAppClient(config)
-    _config = config
   }
 
   return _instance
