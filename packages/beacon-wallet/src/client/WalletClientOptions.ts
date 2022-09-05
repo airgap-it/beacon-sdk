0deccae1b9ac190001fcf6ca3389558196cbbd83
Andreas Gassmann <andreas@andreasgassmann.ch>
2022-09-05 14:32:48 +0100

    feat(): change matrixNodes type

--- packages/beacon-wallet/src/client/WalletClientOptions.ts
+++ packages/beacon-wallet/src/client/WalletClientOptions.ts
@@ -1,4 +1,4 @@
-import { Storage } from '@airgap/beacon-types'
+import { NodeDistributions, Storage } from '@airgap/beacon-types'
 
 /**
  * @category Wallet
@@ -28,7 +28,7 @@ export interface WalletClientOptions {
    * A list of matrix nodes to connect to. If a non-empty array is passed, the default options will be overwritten.
    * One node will be randomly selected based on the local keypair and the other nodes will be used as a fallback in case the primary node goes down.
    *
-   * Only provide the hostname, no https:// prefix. Eg. ['matrix.example.com']
+   * Only provide the hostname, no https:// prefix. Eg. { [Regions.EU1]: ['matrix.example.com'] }
    */
-  matrixNodes?: string[]
+  matrixNodes?: Partial<NodeDistributions>
 }
