9e8bb88bd29582ab561c2887b9068c401bb9e3d7
Andreas Gassmann <andreas@andreasgassmann.ch>
2022-12-30 14:31:01 +0100

    fix(): re-add exports

--- packages/beacon-dapp/src/index.ts
+++ packages/beacon-dapp/src/index.ts
@@ -1,3 +1,10 @@
+export * from '@airgap/beacon-core'
+export * from '@airgap/beacon-transport-matrix'
+export * from '@airgap/beacon-transport-postmessage'
+export * from '@airgap/beacon-types'
+export * from '@airgap/beacon-utils'
+export * from '@airgap/beacon-ui'
+
 import { DAppClient } from './dapp-client/DAppClient'
 import { DAppClientOptions } from './dapp-client/DAppClientOptions'
 import { BeaconEvent, BeaconEventHandler, defaultEventCallbacks } from './events'
