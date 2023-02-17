9a1480a47fedd319221747c9a71cc630f9c4fea3
Miguel Vieira <m.vieira@papers.ch>
2023-02-17 17:13:37 +0000

    feat(): toast component

--- packages/beacon-ui/src/components/toast/index.tsx
+++ packages/beacon-ui/src/components/toast/index.tsx
@@ -1,15 +1,47 @@
-import { Component } from 'solid-js'
+import { Component, For } from 'solid-js'
+import { CloseIcon } from '../icons'
 import Loader from '../loader'
 
 export interface ToastProps {
+  label: string
   open: boolean
+  onClickClose: () => void
+  actions?: { text: string; isBold?: boolean; actionText?: string; actionCallback?: () => void }[]
 }
 
 const Toast: Component<ToastProps> = (props: ToastProps) => {
   return (
     <div class={props.open ? 'toast-wrapper-show' : 'toast-wrapper-hide'}>
-      <Loader />
-      <p>Toast Example</p>
+      <div class="toast-header">
+        <Loader />
+        <p class="toast-label">{props.label}</p>
+        <div class="toast-button-icon" onClick={props.onClickClose}>
+          <CloseIcon />
+        </div>
+      </div>
+      {props.actions && (
+        <div class="toast-body">
+          <For each={props.actions}>
+            {(action) => (
+              <div class="toast-action-wrapper">
+                <p class={`toast-action-label ${action.isBold ? 'toast-action-bold' : ''}`}>
+                  {action.text}
+                </p>
+                {action.actionText && (
+                  <div
+                    class="toast-action-button"
+                    onClick={() => {
+                      if (action.actionCallback) action.actionCallback()
+                    }}
+                  >
+                    {action.actionText}
+                  </div>
+                )}
+              </div>
+            )}
+          </For>
+        </div>
+      )}
     </div>
   )
 }
