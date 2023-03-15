2d522514e1bceb03fd58cf40b1229cb4ca5baff4
Andreas Gassmann <andreas@andreasgassmann.ch>
2023-03-15 15:19:09 +0100

    chore(release): v3.3.5-beta.15

--- packages/beacon-core/src/constants.ts
+++ packages/beacon-core/src/constants.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION: string = '3.3.5-beta.14'
+export const SDK_VERSION: string = '3.3.5-beta.15'
 export const BEACON_VERSION: string = '3'
 
 export const NOTIFICATION_ORACLE_URL: string =
