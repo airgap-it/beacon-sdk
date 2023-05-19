24c07d89651bc6148225206775762a05ecc1f557
Andreas Gassmann <andreas@andreasgassmann.ch>
2023-05-19 12:21:35 +0200

    chore(release): v4.0.4-beta.0-kukai-wc2

--- packages/beacon-core/src/constants.ts
+++ packages/beacon-core/src/constants.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION: string = '4.0.3'
+export const SDK_VERSION: string = '4.0.4-beta.0-kukai-wc2'
 export const BEACON_VERSION: string = '3'
 
 export const NOTIFICATION_ORACLE_URL: string =
