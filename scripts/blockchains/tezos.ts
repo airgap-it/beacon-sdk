7052504d1c21a5c59f7e793fa8c8b935a6f64c32
JulesGuesnon <jules.guesnon@hetic.net>
2023-12-04 16:44:47 +0900

    fix(Tezos): Updated networks to be ghostnet

--- scripts/blockchains/tezos.ts
+++ scripts/blockchains/tezos.ts
@@ -112,20 +112,20 @@ export const tezosWebList: WebApp[] = [
     links: {
       [NetworkType.MAINNET]: 'https://tzsafe.marigold.dev',
       [NetworkType.GHOSTNET]: 'https://ghostnet.tzsafe.marigold.dev',
-      [NetworkType.MONDAYNET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.DAILYNET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.DELPHINET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.EDONET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.FLORENCENET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.GRANADANET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.HANGZHOUNET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.ITHACANET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.JAKARTANET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.KATHMANDUNET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.LIMANET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.MUMBAINET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.NAIROBINET]: 'https://tzsafe.marigold.dev',
-      [NetworkType.OXFORDNET]: 'https://tzsafe.marigold.dev'
+      [NetworkType.MONDAYNET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.DAILYNET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.DELPHINET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.EDONET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.FLORENCENET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.GRANADANET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.HANGZHOUNET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.ITHACANET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.JAKARTANET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.KATHMANDUNET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.LIMANET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.MUMBAINET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.NAIROBINET]: 'https://ghostnet.tzsafe.marigold.dev',
+      [NetworkType.OXFORDNET]: 'https://ghostnet.tzsafe.marigold.dev'
     }
   }
 ]
