bd6de4877a9b0ad4b7cfd0715062e1b1931b532c
JulesGuesnon <jules.guesnon@hetic.net>
2024-01-19 18:29:34 +0900

    fix: reverted change

--- packages/beacon-types/src/types/beacon/messages/ProofOfEventChallengeResponse.ts
+++ packages/beacon-types/src/types/beacon/messages/ProofOfEventChallengeResponse.ts
@@ -2,6 +2,6 @@ import { BeaconBaseMessage, BeaconMessageType } from '@airgap/beacon-types'
 
 export interface ProofOfEventChallengeResponse extends BeaconBaseMessage {
   type: BeaconMessageType.ProofOfEventChallengeResponse
-  signature: string
+  payloadHash: string
   isAccepted: boolean // Indicating whether the challenge is accepted
 }
