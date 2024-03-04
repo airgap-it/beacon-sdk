ca90120c05ba9a81bc5e4953eae063978a61ab81
IsaccoSordo <isacco.sordo@gmail.com>
2024-03-04 10:46:14 +0100

    chore(release): v4.1.3-beta.0

--- packages/beacon-core/src/constants.ts
+++ packages/beacon-core/src/constants.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION: string = '4.1.2'
+export const SDK_VERSION: string = '4.1.3-beta.0'
 export const BEACON_VERSION: string = '3'
 
 export const NOTIFICATION_ORACLE_URL: string =
