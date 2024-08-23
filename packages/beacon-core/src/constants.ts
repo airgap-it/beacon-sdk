105ef0d5a7311f11879b022c1f3b05c65bd1ce18
IsaccoSordo <isacco.sordo@gmail.com>
2024-08-23 11:56:46 +0200

    chore: alpha release

--- packages/beacon-core/src/constants.ts
+++ packages/beacon-core/src/constants.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION: string = '4.2.2'
+export const SDK_VERSION: string = '4.2.3-alpha.0'
 export const BEACON_VERSION: string = '3'
 
 export const NOTIFICATION_ORACLE_URL: string =
