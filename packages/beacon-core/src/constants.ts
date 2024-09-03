491655ae6b89b116810c07ba0a68af46c120fb32
IsaccoSordo <isacco.sordo@gmail.com>
2024-09-03 12:19:46 +0200

    fix: constant

--- packages/beacon-core/src/constants.ts
+++ packages/beacon-core/src/constants.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION: string = '4.3.0-beta.1'
+export const SDK_VERSION: string = '4.3.0'
 export const BEACON_VERSION: string = '3'
 
 export const NOTIFICATION_ORACLE_URL: string =
