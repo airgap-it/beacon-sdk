11071d45871f05d11f9c7ca9f59e26f29ec0f1db
IsaccoSordo <isacco.sordo@gmail.com>
2024-09-05 15:23:56 +0200

    feat: add info alert

--- packages/beacon-ui/src/ui/alert/index.tsx
+++ packages/beacon-ui/src/ui/alert/index.tsx
@@ -7,6 +7,7 @@ import {
 } from '@airgap/beacon-types'
 import { createRoot, Root } from 'react-dom/client'
 import Alert from '../../components/alert'
+import AlertContent from './components/alert-content'
 
 export interface AlertButton {
   label: string
@@ -49,13 +50,13 @@ const closeAlerts = () => {
   closeAlert()
 }
 
-const AlertRoot = () => {
+const AlertRoot = (_props: any) => {
   return (
     <Alert
       open={true}
       loading={false}
       onCloseClick={() => closeAlert()}
-      content={<h1>Hello New World!</h1>}
+      content={<AlertContent title={'test'} body={'test'} data={'test'} />}
     />
   )
 }
