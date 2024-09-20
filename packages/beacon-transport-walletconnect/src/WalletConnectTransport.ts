9d9e736abb203ae649a75e4becd23de629bee90b
IsaccoSordo <isacco.sordo@gmail.com>
2024-09-20 13:55:44 +0200

    fix: removed unused await

--- packages/beacon-transport-walletconnect/src/WalletConnectTransport.ts
+++ packages/beacon-transport-walletconnect/src/WalletConnectTransport.ts
@@ -62,7 +62,7 @@ export class WalletConnectTransport<
 
     this._isConnected = TransportStatus.CONNECTING
 
-    const isLeader = await this.isLeader()
+    const isLeader = this.isLeader()
 
     if (isLeader) {
       await this.client.init()
