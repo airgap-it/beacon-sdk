c6b8b4dcddf8b8df2e8dc6260d3d5f256cfe0ab1
IsaccoSordo <isacco.sordo@gmail.com>
2025-01-07 14:50:45 +0100

    fix: style

--- packages/beacon-ui/src/components/alert/index.tsx
+++ packages/beacon-ui/src/components/alert/index.tsx
@@ -24,7 +24,7 @@ const Alert: React.FC<React.PropsWithChildren<AlertProps>> = (props) => {
           left: '50%',
           transform: 'translate(-50%, -50%)',
           width: 400,
-          bgcolor: 'grey',
+          bgcolor: 'white',
           border: '2px solid #000',
           boxShadow: 24,
           borderRadius: '30px',
