0385106b6c766ce271e40f80a5a1d27b991f7e39
IsaccoSordo <isacco.sordo@gmail.com>
2025-01-13 16:25:46 +0100

    fix: revert

--- packages/beacon-ui/src/ui/alert/index.tsx
+++ packages/beacon-ui/src/ui/alert/index.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import AlertContent from './components/alert-content'
 import { useEffect, useState } from 'react'
 import { Subject } from '../../utils/subject'
 import { AlertConfig } from './common'
@@ -8,15 +7,15 @@ import PairingAlert from './components/pairing-alert'
 let initDone: boolean = false
 const show$ = new Subject<boolean>()
 
-const createAlert = () => {
+const createAlert = (config: AlertConfig) => {
   const el = document.createElement('beacon-modal')
   document.body.prepend(el)
-  setTimeout(() => createRoot(el).render(<AlertRoot />), 50)
+  setTimeout(() => createRoot(el).render(<AlertRoot {...config} />), 50)
   initDone = true
 }
 
-const openAlert = (_config: AlertConfig) => {
-  !initDone && createAlert()
+const openAlert = (config: AlertConfig) => {
+  !initDone && createAlert(config)
   show$.next(true)
 }
 
@@ -28,23 +27,12 @@ const closeAlerts = () => {
   closeAlert()
 }
 
-const AlertRoot = (_props: any) => {
+const AlertRoot = (props: AlertConfig) => {
   const [isAlertVisible, setIsAlertVisible] = useState(true)
   useEffect(() => {
     show$.subscribe((value) => setIsAlertVisible(value))
   }, [])
-  return (
-    <>
-      {isAlertVisible && (
-        <PairingAlert
-          open={true}
-          loading={false}
-          onClose={() => closeAlert()}
-          content={<AlertContent />}
-        />
-      )}
-    </>
-  )
+  return <>{isAlertVisible && <PairingAlert {...props} closeButtonCallback={closeAlert} />}</>
 }
 
 export { openAlert, closeAlert, closeAlerts }
