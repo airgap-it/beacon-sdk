43b7e88f8a02e1085a904f2aa83742980ce4a764
IsaccoSordo <isacco.sordo@gmail.com>
2025-01-16 16:44:56 +0100

    fix: improved dragging

--- packages/beacon-ui/src/components/toast/index.tsx
+++ packages/beacon-ui/src/components/toast/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { useDrag } from '@use-gesture/react'
 import { CloseIcon } from '../icons'
 import Loader from '../loader'
 import { isMobileOS } from '../../utils/platform'
@@ -38,15 +39,16 @@ function parseWallet(
 
 const Toast: React.FC<ToastProps> = (props: ToastProps) => {
   const [showMoreInfo, setShowMoreInfo] = useState<boolean>(true)
-  const [divPosition, setDivPosition] = useState<{ x: number; y: number }>({
-    x: isMobileOS(window) ? 12 : window.innerWidth - 460,
-    y: 12
-  })
-  const [isDragging, setIsDragging] = useState<boolean>(false)
-  const offset = { x: 0, y: 0 }
-
   const hasWalletObject = props.label.includes('{{wallet}}') && props.walletInfo
   const isRequestSentToast = props.label.includes('Request sent to')
+
+  // Track the toast position (x, y)
+  const [[x, y], setPosition] = useState<[number, number]>([0, 0])
+
+  const bindDrag = useDrag(({ offset: [mx, my] }) => {
+    // Update x, y whenever offset changes
+    setPosition([mx, my])
+  })
 
   useEffect(() => {
     if (isRequestSentToast) {
@@ -57,70 +59,33 @@ const Toast: React.FC<ToastProps> = (props: ToastProps) => {
     }
   }, [isRequestSentToast])
 
-  const onMouseDownHandler = (event: React.MouseEvent<HTMLDivElement>) => {
-    event.preventDefault() // prevents inner text highlighting
-    const target = event.target as HTMLElement
-
-    if (target.className !== 'toast-header' && target.parentElement?.className !== 'toast-header') {
-      return
-    }
-
-    const boundingRect = target.getBoundingClientRect()
-    offset.x = event.clientX - boundingRect.x
-    offset.y = event.clientY - boundingRect.y
-    setIsDragging(true)
-  }
-
-  const onMouseMoveHandler = (event: MouseEvent) => {
-    if (isDragging && event.buttons === 1) {
-      const newX = Math.min(Math.max(event.clientX - offset.x, 0), window.innerWidth - 460)
-      const newY = Math.min(Math.max(event.clientY - offset.y, 0), window.innerHeight - 12)
-
-      setDivPosition({
-        x: newX,
-        y: newY
-      })
-    }
-  }
-
-  const onMouseUpHandler = () => {
-    setIsDragging(false)
-  }
-
-  useEffect(() => {
-    if (isDragging) {
-      window.addEventListener('mousemove', onMouseMoveHandler)
-      window.addEventListener('mouseup', onMouseUpHandler)
-    } else {
-      window.removeEventListener('mousemove', onMouseMoveHandler)
-      window.removeEventListener('mouseup', onMouseUpHandler)
-    }
-
-    return () => {
-      window.removeEventListener('mousemove', onMouseMoveHandler)
-      window.removeEventListener('mouseup', onMouseUpHandler)
-    }
-  }, [isDragging])
-
   return (
     <div
-      style={{ left: `${divPosition.x}px`, top: `${divPosition.y}px` }}
+      style={{
+        position: 'absolute',
+        transform: `translate3d(${x}px, ${y}px, 0)`,
+        // Required
+        touchAction: 'none'
+      }}
       className={props.open ? 'toast-wrapper-show' : 'toast-wrapper-hide'}
-      onMouseDown={onMouseDownHandler}
+      {...bindDrag()}
     >
       <div className="toast-header">
-        {!isDragging && <Loader />}
+        <Loader />
         {hasWalletObject && props.walletInfo && <>{parseWallet(props.label, props.walletInfo)}</>}
         {!hasWalletObject && <p className="toast-label">{props.label}</p>}
+
         {!isMobileOS(window) && props.openWalletAction && (
           <div className="toast-action-button" onClick={props.openWalletAction}>
             Open Wallet
           </div>
         )}
+
         <div className="toast-button-icon" onClick={props.onClickClose}>
           <CloseIcon />
         </div>
       </div>
+
       {props.actions && showMoreInfo && (
         <div className="toast-body">
           {props.actions.map((action, index) => (
