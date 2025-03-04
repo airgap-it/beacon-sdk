bd8666d5354b86b887e2c192f82ce7dd8ba89632
IsaccoSordo <isacco.sordo@gmail.com>
2025-03-04 16:16:48 +0100

    fix: phlo

--- packages/beacon-ui/src/ui/alert/index.tsx
+++ packages/beacon-ui/src/ui/alert/index.tsx
@@ -37,7 +37,7 @@ const openBugReport = () => {
       walletConnectSyncCode: '',
       p2pSyncCode: '',
       postmessageSyncCode: '',
-      networkType: NetworkType.GHOSTNET
+      networkType: NetworkType.MAINNET
     }
   })
 }
