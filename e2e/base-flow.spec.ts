625eb302aca2117bab0d9a93512c738f6c118998
IsaccoSordo <isacco.sordo@gmail.com>
2025-05-30 18:56:17 +0200

    feat: add pair other tests

--- e2e/base-flow.spec.ts
+++ e2e/base-flow.spec.ts
@@ -113,3 +113,77 @@ test('should display Temple Wallet', async ({ browser }) => {
 
   // TODO extension pairing??
 })
+
+test('should pair other with Beacon', async ({ browser }) => {
+  // --- setup context + grant clipboard permissions ---
+  const dappCtx = await browser.newContext()
+  await dappCtx.grantPermissions(['clipboard-read', 'clipboard-write'], {
+    origin: 'http://localhost:1234'
+  })
+
+  const dapp = await dappCtx.newPage()
+  await dapp.goto('http://localhost:1234/dapp.html')
+
+  // --- trigger the Beacon pairing alert ---
+  await dapp.click('#requestPermission')
+  await dapp.waitForSelector('div.alert-wrapper-show', { state: 'visible', timeout: 5_000 })
+
+  // --- choose AirGap and wait for QR display ---
+  await dapp.click('div.alert-footer')
+  await dapp.click('button:has-text("Show QR code")')
+  await dapp.waitForSelector('span.pair-other-info', { state: 'visible', timeout: 5_000 })
+
+  await dapp.click('button:has-text("Beacon")')
+
+  await dapp.waitForSelector('div.qr-right', { state: 'visible', timeout: 5_000 })
+
+  // --- click the QR element to copy the pairing code ---
+  await dapp.click('div.qr-right')
+
+  // --- read back from the clipboard in the page context ---
+  const pairingCode = await dapp.evaluate(async () => {
+    return await navigator.clipboard.readText()
+  })
+
+  if (typeof pairingCode !== 'string') {
+    console.error('Wrong pairing code:', pairingCode)
+    throw new Error('QR code copy failed.')
+  }
+})
+
+test('should pair other with WalletConnect', async ({ browser }) => {
+  // --- setup context + grant clipboard permissions ---
+  const dappCtx = await browser.newContext()
+  await dappCtx.grantPermissions(['clipboard-read', 'clipboard-write'], {
+    origin: 'http://localhost:1234'
+  })
+
+  const dapp = await dappCtx.newPage()
+  await dapp.goto('http://localhost:1234/dapp.html')
+
+  // --- trigger the Beacon pairing alert ---
+  await dapp.click('#requestPermission')
+  await dapp.waitForSelector('div.alert-wrapper-show', { state: 'visible', timeout: 5_000 })
+
+  // --- choose AirGap and wait for QR display ---
+  await dapp.click('div.alert-footer')
+  await dapp.click('button:has-text("Show QR code")')
+  await dapp.waitForSelector('span.pair-other-info', { state: 'visible', timeout: 5_000 })
+
+  await dapp.click('button:has-text("WalletConnect")')
+
+  await dapp.waitForSelector('div.qr-right', { state: 'visible', timeout: 5_000 })
+
+  // --- click the QR element to copy the pairing code ---
+  await dapp.click('div.qr-right')
+
+  // --- read back from the clipboard in the page context ---
+  const pairingCode = await dapp.evaluate(async () => {
+    return await navigator.clipboard.readText()
+  })
+
+  if (typeof pairingCode !== 'string') {
+    console.error('Wrong pairing code:', pairingCode)
+    throw new Error('QR code copy failed.')
+  }
+})
