dc62ff0c9d75bed5dd010c5f00009ef9254c5603
Lukas Schönbächler <l.schoenbaechler@papers.ch>
2025-11-26 00:27:17 +0100

    chore(release): version 5.0.0-beta.1

--- packages/beacon-core/src/constants.ts
+++ packages/beacon-core/src/constants.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION: string = '5.0.0-alpha.4'
+export const SDK_VERSION: string = '5.0.0-beta.1'
 export const BEACON_VERSION: string = '3'
 export const PROTOCOL_VERSION_V1 = 1
 export const PROTOCOL_VERSION_V2 = 2
